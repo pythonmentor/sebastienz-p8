69093c8e4b1e2f1c7f4f2034db95384d15d41500
Mystic67 <webmaster@passion-geek.com>
2019-10-15 18:56:32 +0200

    Add close session when close webbrowser.

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -30,6 +30,9 @@ else:
     DEBUG = True
     ALLOWED_HOSTS = ['127.0.0.1']
 
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+#SESSION_COOKIE_AGE=
+
 
 # Application definition
 
@@ -129,3 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'accounts/static/'),
+    os.path.join(BASE_DIR, 'store/static/'),
+]
