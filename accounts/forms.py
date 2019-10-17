ef32427c999a16ed5e02b2b02158c72b5ff30821
Mystic67 <webmaster@passion-geek.com>
2019-10-17 22:05:41 +0200

    Create AccountForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,15 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from django.forms import ModelForm
 
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(label="Pseudo",
                                widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "pseudo"}))
     password = forms.CharField(label="Mot de passe",
-                                widget=forms.PasswordInput(
-                                    attrs={'class': 'form-control', 'placeholder': 'mot de passe'}))
+                               widget=forms.PasswordInput(
+                                   attrs={'class': 'form-control', 'placeholder': 'mot de passe'}))
 
 
 class RegisterForm(UserCreationForm):
@@ -18,7 +19,7 @@ class RegisterForm(UserCreationForm):
                                 widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'nom'}))
     first_name = forms.CharField(label="Prénom",
                                  widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'prénom'}))
-    email = forms.EmailField(label="Adresse Email", required=True,
+    email = forms.EmailField(label="Adresse électronique :", required=True,
                              widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'email'}))
     password1 = forms.CharField(label="Mot de passe",
                                 widget=forms.PasswordInput(
@@ -30,3 +31,18 @@ class RegisterForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = UserCreationForm.Meta.fields + ('username', 'last_name', 'first_name', 'email', 'password1')
+
+
+class AccountForm(ModelForm):
+    username = forms.CharField(label="Pseudo",
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "pseudo"}))
+    last_name = forms.CharField(label="Nom",
+                                widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'nom'}))
+    first_name = forms.CharField(label="Prénom",
+                                 widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'prénom'}))
+    email = forms.EmailField(label="Adresse électronique :", required=True,
+                             widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'email'}))
+
+    class Meta(ModelForm):
+        model = User
+        fields = ('username', 'last_name', 'first_name', 'email')
