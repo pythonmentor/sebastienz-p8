228e9b6a22e15ccbc6da8d87ee7c2f3c1e599f40
Mystic67 <webmaster@passion-geek.com>
2019-10-22 14:25:56 +0200

    Add JQuery toggle button functions to activate/desactivate account edition mode

--- accounts/static/accounts/js/accounts.js
+++ accounts/static/accounts/js/accounts.js
@@ -1,25 +1,11 @@
 $(function(){
 
+    //************************** Resize masterhead ******************************************************************
     function resize_masthead() {
         var title = $(document).prop('title');
         if (title !== "Pur Beurre - Accueil") {
             $('.masthead').height($('form').height()+ $('.navbar').height()+ $('h1').height()+ $('footer').height());
         }
-    }
-    $(function() {
-        $('#edit-toggle').bootstrapToggle({
-            on: 'Activé',
-            off: 'Désactivé'
-        });
-    });
-
-    if ($('#edit-toggle').attr('checked')=== true){
-        // $('#account .form-control').removeAttr('disabled');
-        $('#account .form-control').attr('disabled', true);
-    }
-    else{
-         $('#account .form-control').attr('disabled', true);
-        //$('#account .form-control').removeAttr('disabled');
     }
 
     $(window).resize(function(){
@@ -28,13 +14,27 @@ $(function(){
 
     $(document).ready(function(){
         resize_masthead();
-        //$('#account .form-control').attr('disabled', true);
     });
 
-    $('#logout').on('click', function(){
-
+    //****************** Toggle button for account edition mode ******************************************************
+    $(function() {
+        $('#edit-toggle').bootstrapToggle({
+            on: 'Activé',
+            off: 'Désactivé'
+        });
     });
-
-
+    //***************** Activate/Désactivate edition mode for account page *******************************************
+    $('#edition').click(function () {
+        console.log('cliqué');
+        if ($('#edition .toggle').hasClass('off')) {
+            console.log("ON");
+            $('#account .form-control').attr('disabled', false);
+            $('#save-btn').removeClass('d-none');
+        } else {
+            console.log("OFF");
+            $('#account .form-control').attr('disabled', true);
+            $('#save-btn').addClass('d-none');
+        }
+    });
 
 });
