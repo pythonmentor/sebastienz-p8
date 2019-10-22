5a43775b4dec117d8b6e88e016923eaf314ab933
Mystic67 <webmaster@passion-geek.com>
2019-10-22 14:15:27 +0200

    Add CustonUserModel with Authenticate username formated

--- accounts/models.py
+++ accounts/models.py
@@ -1,16 +1,22 @@
+import re
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-# Create your models here.
-# class Customer(models.Model):
-#     pseudo = models.CharField(verbose_name="Pseudo", max_length=50)
-#     name = models.CharField(verbose_name="Nom", max_length=50, blank=True)
-#     first_name = models.CharField(verbose_name="Prénom", max_length=50, blank=True)
-#     email = models.EmailField(verbose_name="Adresse mail", max_length=100)
-#     password = models.CharField(verbose_name="Mot de passe", max_length=50)
-#
-#     def __str__(self):
-#         return self.first_name + " " + self.name
-#
-#     class META:
-#         verbose_name = "Client"
+class CustomUser(AbstractUser):
+    nickname = models.CharField(verbose_name="Pseudo", blank=True, unique=False, max_length=50)
+    last_name = models.CharField(verbose_name="Nom", max_length=150, blank=True)
+    first_name = models.CharField(verbose_name="Prénom", max_length=150, blank=True)
+    email = models.EmailField(verbose_name="Adresse mail", blank=False, unique=True, max_length=150)
+    password = models.CharField(verbose_name="Mot de passe", max_length=150)
+
+    def __str__(self):
+        return self.email
+
+    def formated_username(self):
+        regex = r"\w+"
+        return re.findall(regex, self.username)[0]
+
+    class META:
+        verbose_name = "Client"
