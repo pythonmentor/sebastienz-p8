e33d1088482a9a72ba4bfcf24f20372ef99b68fa
Mystic67 <webmaster@passion-geek.com>
2019-11-15 16:38:02 +0100

    Change user favorites substitutes admin registration

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import User,Favorites
+from .models import User, User_Favorites_Substitutes
 
 admin.site.register(User)
-admin.site.register(Favorites)
-
+admin.site.register(User_Favorites_Substitutes)
--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Categories, Products, Nutriments_for_100g
 
-# Register your models here.
+admin.site.register(Categories)
+admin.site.register(Products)
+admin.site.register(Nutriments_for_100g)
