e5363ac267b38929a880afe4c281b7a106ebf104
Mystic67 <webmaster@passion-geek.com>
2019-11-15 16:57:00 +0100

    Remove unused imports

--- store/forms.py
+++ store/forms.py
@@ -1,11 +1,10 @@
-from django.forms import ModelForm, TextInput, EmailInput, PasswordInput
 from django import forms
-from django.forms.utils import ErrorList
-#from .models import Customer
+
 
 class SearchForm(forms.Form):
-    search_product = forms.CharField(label='', max_length = 100, widget=forms.TextInput(attrs={
-            'placeholder': 'Produit',
-            'id':'search_form',
-            'class':'rounded col-md-6 col-md-offset-3'
-            }))
+    search_product = forms.CharField(label='', max_length=100, widget=forms.TextInput(attrs={
+        'placeholder': 'Produit',
+        'id': 'search_form',
+        'class': 'rounded col-md-6 col-md-offset-3'
+    }))
+
