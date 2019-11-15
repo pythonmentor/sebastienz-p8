fd2e3acd703382bfce8455ba3c3084b11899cd11
Mystic67 <webmaster@passion-geek.com>
2019-11-15 17:00:19 +0100

    Add URLS for products, substitutes and details pages

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
+from django.urls import path, re_path
+
 from . import views
 
 app_name = "store"
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('products/', views.products, name='products'),
+    path('<int:product_id>/substitutes/', views.substitutes, name='substitutes'),
+    path('<int:product_id>/', views.details, name='details')
 ]
