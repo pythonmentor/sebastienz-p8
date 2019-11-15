2825c6dcd0201f295c1169e7808ae20ddde3cf99
Mystic67 <webmaster@passion-geek.com>
2019-11-15 20:51:52 +0100

    Register User Favorites Substitutes to admin

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Categories, Products, Nutriments_for_100g
+from .models import Categories, Products, Nutriments_for_100g, User_Favorites_Substitutes
 
 admin.site.register(Categories)
 admin.site.register(Products)
 admin.site.register(Nutriments_for_100g)
+admin.site.register(User_Favorites_Substitutes)
