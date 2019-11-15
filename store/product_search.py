0c96e4d15e4fa873a423b377d5675525e22c3c52
Mystic67 <webmaster@passion-geek.com>
2019-11-15 16:50:34 +0100

    Add class with method to found substitutes for a products with maximum categories in common.

---
+++ store/product_search.py
@@ -0,0 +1,31 @@
+from .models import Products
+
+
+class ProductSearch:
+
+    @classmethod
+    def found_substitutes(cls, product_id):
+        # find product in DB
+        product = Products.objects.get(pk=product_id)
+        # find all products (substitutes) which are in same categories as initial product
+        substitutes_ids_list = []
+        dic = {}
+        # List all substitutes in all common categories with better nutrition grade
+        for cat in product.categories.all():
+            substitutes_ids_list.extend([prod.id for prod in Products.objects.filter(categories__name=cat).
+                                    filter(nutrition_grade_fr__lt=product.nutrition_grade_fr)])
+        # Put temporarily each unique substitutes ids from list in dic,
+        # with number of common instance with oder categories
+        if len(substitutes_ids_list) > 0:
+            for sub_id in substitutes_ids_list:
+                if sub_id not in dic:
+                    dic[sub_id] = substitutes_ids_list.count(sub_id)
+
+        sorted_id_list = [key[0] for key in sorted(dic.items(), key=lambda x: x[1], reverse=True)]
+
+        # return the six best substitutes from dictionnary and return them in queryset
+        # Define ordering to preserve list order
+        SQL_clauses = ' '.join(['WHEN id=%s THEN %s' % (pk, i) for i, pk in enumerate(sorted_id_list)])
+        ordering = 'CASE %s END' % SQL_clauses
+        return Products.objects.filter(id__in=sorted_id_list[:6]).extra(
+           select={'ordering': ordering}, order_by=('ordering',))
