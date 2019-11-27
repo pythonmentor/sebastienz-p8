0fa22174d6781dfb55dd8c51a994bf91556a4506
Mystic67 <webmaster@passion-geek.com>
2019-11-27 18:36:10 +0100

    Change search design and change some bootstrap properties for hight screen resolution

--- store/forms.py
+++ store/forms.py
@@ -3,8 +3,8 @@ from django import forms
 
 class SearchForm(forms.Form):
     search_product = forms.CharField(label='', max_length=100, widget=forms.TextInput(attrs={
-        'placeholder': 'Produit',
+        'placeholder': 'Rechercher',
         'id': 'search_form',
-        'class': 'rounded col-md-6 col-md-offset-3','autofocus':'true'
+        'class': 'form-control col-8 offset-2 rounded-left', 'autofocus': 'true'
     }))
 
