f5f6601fdf42658bc9aa536e402fb18ca9138982
Mystic67 <webmaster@passion-geek.com>
2019-11-27 21:52:51 +0100

    Add settings for production

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -132,10 +132,27 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'accounts/static/'),
-    os.path.join(BASE_DIR, 'store/static/'),
-]
+if os.environ.get('ENV') == 'PRODUCTION':
+
+    # Static files settings
+    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+    # Extra places for collectstatic to find static files.
+    STATICFILES_DIRS = (
+        os.path.join(PROJECT_ROOT, 'static'),
+    )
+
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+else:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'accounts/static/'),
+        os.path.join(BASE_DIR, 'store/static/'),
+    ]
 
 # Openfoofacts search catégories
 CATEGORIES = [
