19e5bdc52e897d06fa2c6731fa1d4ec1cb9acea0
Mystic67 <webmaster@passion-geek.com>
2019-11-30 23:30:44 +0100

    Change settings for production

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -133,23 +133,20 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'accounts/static/'),
-    os.path.join(BASE_DIR, 'store/static/'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'accounts/static/'),
+#     os.path.join(BASE_DIR, 'store/static/'),
+# ]
 
 INTERNAL_IPS = ['127.0.0.1']
 
 if os.environ.get('ENV') == 'PRODUCTION':
 
-    # Static files settings
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'pur_beurre/staticfiles')
 
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
+        os.path.join(BASE_DIR, 'pur_beurre/static'),
     )
 
     # Simplified static file serving.
